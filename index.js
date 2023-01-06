1da94634741a5f6571620b1488e99c65224cfe28
FaizanPotrick <faizan.potrick02@gmail.com>
2023-01-06 07:24:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const cors = require("cors");
+const express = require("express");
+const { createServer } = require("https");
+const { Server } = require("socket.io");
+// (http, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
+const app = express();
+const http = createServer(app);
+const io = new Server(http, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(require("./router"));
+
+let rooms = {};
+let messages = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-room", (room, user) => {
+    socket.join(room);
+    rooms[room] = rooms[room] || { users: {} };
+    rooms[room].users[socket.id] = user;
+    io.to(room).emit("room", messages[room] || "");
+  });
+  socket.on("send", (room, message) => {
+    messages[room] = message;
+    socket.to(room).emit("receive", message);
+  });
+});
+
+http.listen(process.env.PORT || 5000);
