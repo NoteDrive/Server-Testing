d00b7a8ad16f8ae36ed6ba841503e0840cb417c9
FaizanPotrick <faizan.potrick02@gmail.com>
2023-01-06 08:43:53 +0530

    add

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const cors = require("cors");
 const express = require("express");
 const { createServer } = require("http");
-const socketIo = require("socket.io");
+const { Server } = require("socket.io");
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 app.use(cors());
-// app.use(require("./router"));
+app.use(require("./router"));
 
 const http = createServer(app);
-const io = socketIo(http, {
+const io = new Server(http, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
